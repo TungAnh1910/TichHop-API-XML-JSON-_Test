34723637e2fc1c6239d77b948f1c5125c5b23211
TungAnh1910 <102455632+TungAnh1910@users.noreply.github.com>
2023-01-05 21:56:29 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Check02001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHs = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgrDanhSach = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.btnDanhSach = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrDanhSach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ma NV";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(170, 53);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(223, 22);
+            this.txtMa.TabIndex = 1;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(170, 103);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(223, 22);
+            this.txtTen.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ten NV";
+            // 
+            // txtHs
+            // 
+            this.txtHs.Location = new System.Drawing.Point(170, 152);
+            this.txtHs.Name = "txtHs";
+            this.txtHs.Size = new System.Drawing.Size(223, 22);
+            this.txtHs.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "HS Luong";
+            // 
+            // dgrDanhSach
+            // 
+            this.dgrDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrDanhSach.Location = new System.Drawing.Point(122, 214);
+            this.dgrDanhSach.Name = "dgrDanhSach";
+            this.dgrDanhSach.RowHeadersWidth = 51;
+            this.dgrDanhSach.RowTemplate.Height = 24;
+            this.dgrDanhSach.Size = new System.Drawing.Size(526, 150);
+            this.dgrDanhSach.TabIndex = 6;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(597, 97);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 7;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(597, 132);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 8;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(597, 171);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(470, 393);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(75, 23);
+            this.btnTim.TabIndex = 10;
+            this.btnTim.Text = "Tim Kiem";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Nhap ma";
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(170, 393);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(223, 22);
+            this.txtNhap.TabIndex = 12;
+            // 
+            // btnDanhSach
+            // 
+            this.btnDanhSach.Location = new System.Drawing.Point(597, 53);
+            this.btnDanhSach.Name = "btnDanhSach";
+            this.btnDanhSach.Size = new System.Drawing.Size(100, 23);
+            this.btnDanhSach.TabIndex = 13;
+            this.btnDanhSach.Text = "Danh sach";
+            this.btnDanhSach.UseVisualStyleBackColor = true;
+            this.btnDanhSach.Click += new System.EventHandler(this.btnDanhSach_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDanhSach);
+            this.Controls.Add(this.txtNhap);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgrDanhSach);
+            this.Controls.Add(this.txtHs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgrDanhSach)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgrDanhSach;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.Button btnDanhSach;
+    }
+}
+
---
+++ NVController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Check01001.Controllers
+{
+    public class NVController : ApiController
+    {
+        [HttpGet]
+        public List<NV> Layds()
+        {
+            NhanVienDataContext context = new NhanVienDataContext();
+            List<NV> dsnv = context.NVs.ToList();
+            return dsnv;
+        }
+        [HttpPost]
+        public bool Them(string ma, string ten, string hs)
+        {
+            try
+            {
+                NhanVienDataContext context = new NhanVienDataContext();
+                NV nv = new NV();
+                nv.MaNV = ma;
+                nv.TenNV = ten;
+                nv.HSLuong = hs;
+                context.NVs.InsertOnSubmit(nv);
+                context.SubmitChanges();
+                return true;
+            }
+            catch { }
+            return false;
+        }
+        [HttpDelete]
+        public bool Xoa(string ma)
+        {
+            NhanVienDataContext context = new NhanVienDataContext();
+            NV nv = context.NVs.FirstOrDefault(x => x.MaNV == ma);
+            if (nv != null)
+            {
+                context.NVs.DeleteOnSubmit(nv);
+                context.SubmitChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
